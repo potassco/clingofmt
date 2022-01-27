92dbb685ffd9f7907056c50816d27b2584870257
Sven Thiele <sthiele78@gmail.com>
2022-01-27 23:50:22 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
     path::PathBuf,
 };
 
-const MAX_LENGTH: usize = 40;
+const MAX_LENGTH: usize = 60;
 
 /// Format clingo code
 #[derive(Parser, Debug)]
@@ -61,40 +61,27 @@ fn main() {
     }
 }
 
-fn flush(buf: &mut String, ident_level: usize) -> Result<()> {
-    let mut stdout = std::io::stdout();
-    writeln!(&mut stdout, "{buf}")?;
-    buf.clear();
-    for _i in 0..ident_level {
-        buf.push_str("    ");
-    }
-    Ok(())
-}
-fn plush(buf: &mut String) -> Result<()> {
-    let mut stdout = std::io::stdout();
-    write!(&mut stdout, "{buf}")?;
+fn flush(out: &mut dyn Write, buf: &mut String) -> Result<()> {
+    write!(out, "{buf}")?;
     buf.clear();
     Ok(())
 }
-fn hush(buf: &mut String, ident_level: usize) -> Result<()> {
-    let mut stdout = std::io::stdout();
-    for _i in 0..ident_level {
-        write!(&mut stdout, "    ")?;
-    }
-    writeln!(&mut stdout, "{buf}")?;
+fn flushln(out: &mut dyn Write, buf: &mut String) -> Result<()> {
+    writeln!(out, "{buf}")?;
     buf.clear();
     Ok(())
 }
-fn lush(buf: &mut String, ident_level: usize) -> Result<()> {
-    let mut stdout = std::io::stdout();
-    for _i in 0..ident_level {
-        write!(&mut stdout, "    ")?;
-    }
-    write!(&mut stdout, "{buf}")?;
-    buf.clear();
-    Ok(())
+fn flushln_indent(out: &mut dyn Write, buf: &mut String, indent_level: usize) -> Result<()> {
+    flushln(out, buf)?;
+    indent(out, indent_level)
 }
 
+fn indent(out: &mut dyn Write, ident_level: usize) -> Result<()> {
+    for _i in 0..ident_level {
+        write!(out, "    ")?;
+    }
+    Ok(())
+}
 fn run() -> Result<()> {
     let opt = Opt::parse();
 
@@ -102,17 +89,27 @@ fn run() -> Result<()> {
     let source_code =
         fs::read(path).with_context(|| format!("Error reading source file {}", path))?;
 
-    let mut stdout = std::io::stdout();
-
     let mut parser = tree_sitter::Parser::new();
     parser
         .set_language(tree_sitter_clingo::language())
         .expect("Error loading clingo grammar");
-
     let tree = parser.parse(&source_code, None).unwrap();
+
+    let mut buf = std::io::stdout();
+
+    // error_pass(&tree, path)?;
+    pass_one(&tree, &source_code, &mut buf, opt.debug)
+}
+
+fn pass_one(
+    tree: &tree_sitter::Tree,
+    source_code: &[u8],
+    out: &mut dyn Write,
+    debug: bool,
+) -> Result<()> {
     let mut cursor = tree.walk();
 
-    let mut first = true;
+    let mut nl = false;
     let mut buf = String::new();
     let mut indent_level = 0;
     let mut mindent_level = 0;
@@ -132,20 +129,12 @@ fn run() -> Result<()> {
             // what happens after the element
             if is_named {
                 match node.kind() {
-                    "comment" => {
-                        if in_statement {
-                            hush(&mut buf, mindent_level)?;
-                        } else {
-                            flush(&mut buf, mindent_level)?;
-                        }
-                        first = true;
-                    }
                     "statement" => {
                         if !has_head || has_body {
-                            writeln!(&mut stdout, "{buf}")?;
+                            writeln!(out, "{buf}")?;
                         } else {
                             // no newline after facts.
-                            write!(&mut stdout, "{buf}")?;
+                            write!(out, "{buf}")?;
                         }
                         buf.clear();
 
@@ -161,20 +150,8 @@ fn run() -> Result<()> {
                         in_ntermvec -= 1;
                     }
                     "NOT" | "aggregatefunction" => buf.push(' '),
-                    "IF" => {
-                        mindent_level += 1; // decrease after bodydot
-                        if !has_head {
-                            buf.push(' ');
-                        } else {
-                            flush(&mut buf, mindent_level)?;
-                        }
-                    }
                     "bodydot" => {
                         mindent_level -= 1;
-                    }
-                    "lubodyaggregate" => {}
-                    "bodyaggrelem" => {
-                        // mindent_level -= 1;
                     }
                     "noptcondition" => {
                         in_noptcondition = false;
@@ -184,36 +161,63 @@ fn run() -> Result<()> {
                         in_conjunction = false;
                         mindent_level -= 1;
                     }
-                    "COMMA" => {
-                        if in_ntermvec == 0 {
-                            flush(&mut buf, mindent_level)?;
-                        } else if buf.len() >= MAX_LENGTH {
-                            flush(&mut buf, mindent_level)?;
-                        } else {
-                            buf.push(' ');
-                        }
-                    }
-                    "SEM" => {
-                        flush(&mut buf, mindent_level)?;
-                    }
-                    "COLON" => {
-                        if in_conjunction {
-                            mindent_level += 1;
-                        }
-                        if in_noptcondition {
-                            mindent_level += 1;
-                        }
-                        flush(&mut buf, mindent_level)?;
-                    }
-                    "LBRACE" => {
-                        mindent_level += 1;
-                        flush(&mut buf, mindent_level)?;
-                    }
                     "LPAREN" => {
                         mindent_level += 1;
                     }
                     "SHOW" | "cmp" => buf.push(' '),
                     _ => {}
+                }
+                if node.child_count() == 0 {
+                    match node.kind() {
+                        "comment" => {
+                            if in_statement && nl {
+                                writeln!(out)?;
+                                indent(out, mindent_level)?;
+                            }
+                            flushln_indent(out, &mut buf, mindent_level)?;
+
+                            nl = false;
+                        }
+                        "SEM" => {
+                            flushln_indent(out, &mut buf, mindent_level)?;
+                            nl = false;
+                        }
+                        "COLON" => {
+                            if in_conjunction {
+                                mindent_level += 1;
+                            }
+                            if in_noptcondition {
+                                mindent_level += 1;
+                            }
+                            flushln_indent(out, &mut buf, mindent_level)?;
+                            nl = false;
+                        }
+                        "LBRACE" => {
+                            mindent_level += 1;
+                            flushln_indent(out, &mut buf, mindent_level)?;
+                            nl = false;
+                        }
+                        "COMMA" => {
+                            if in_ntermvec == 0 || buf.len() >= MAX_LENGTH {
+                                flushln_indent(out, &mut buf, mindent_level)?;
+                                nl = false;
+                            } else {
+                                buf.push(' ');
+                            }
+                        }
+                        "IF" => {
+                            mindent_level += 1; // decrease after bodydot
+                            if !has_head {
+                                buf.push(' ');
+                            } else {
+                                flushln_indent(out, &mut buf, mindent_level)?;
+                                nl = false;
+                            }
+                        }
+                        _ => {
+                            nl = true;
+                        }
+                    };
                 }
             }
             if cursor.goto_next_sibling() {
@@ -230,21 +234,18 @@ fn run() -> Result<()> {
                 match node.kind() {
                     "comment" => {
                         if in_statement {
-                            plush(&mut buf)?;
+                            flush(out, &mut buf)?;
                         } else {
-                            if !first {
-                                writeln!(&mut stdout)?;
-                            } else {
-                                first = false
+                            if nl {
+                                writeln!(out)?;
                             }
-                            lush(&mut buf, mindent_level)?;
+                            indent(out, mindent_level)?;
+                            flush(out, &mut buf)?;
                         }
                     }
                     "statement" => {
-                        if !first {
-                            writeln!(&mut stdout)?;
-                        } else {
-                            first = false
+                        if nl {
+                            writeln!(out)?;
                         }
                         in_statement = true;
                     }
@@ -262,14 +263,13 @@ fn run() -> Result<()> {
                     "ntermvec" => {
                         in_ntermvec += 1;
                     }
-                    "lubodyaggregate" => {}
                     "IF" => {
                         buf.push(' ');
                     }
                     "COLON" | "cmp" => buf.push(' '),
                     "RBRACE" => {
                         mindent_level -= 1;
-                        flush(&mut buf, mindent_level)?;
+                        flushln_indent(out, &mut buf, mindent_level)?;
                     }
                     "RPAREN" => {
                         mindent_level -= 1;
@@ -283,12 +283,12 @@ fn run() -> Result<()> {
                     let text = std::str::from_utf8(&source_code[start_byte..end_byte]).unwrap();
 
                     buf.push_str(text);
-                    if opt.debug {
+                    if debug {
                         debug!("{} ", text);
                     }
                 }
 
-                if opt.debug {
+                if debug {
                     let indent = "  ".repeat(indent_level);
                     let start = node.start_position();
                     let end = node.end_position();
@@ -313,7 +313,7 @@ fn run() -> Result<()> {
 
                 buf.push_str(text);
 
-                if opt.debug {
+                if debug {
                     debug!("{}", text);
                     let indent = "  ".repeat(indent_level);
                     let start = node.start_position();
@@ -341,44 +341,48 @@ fn run() -> Result<()> {
             }
         }
     }
-    cursor.reset(tree.root_node());
-
-    let mut first_error = None;
-    loop {
-        let node = cursor.node();
-        if node.has_error() {
-            if node.is_error() || node.is_missing() {
-                first_error = Some(node);
-                break;
-            } else if !cursor.goto_first_child() {
-                break;
-            }
-        } else if !cursor.goto_next_sibling() {
-            break;
-        }
-    }
-
-    if first_error.is_some() {
-        error!("{}", path);
-        if let Some(node) = first_error {
-            let start = node.start_position();
-            let end = node.end_position();
-            error!("\t(");
-            if node.is_missing() {
-                if node.is_named() {
-                    error!("MISSING {}", node.kind());
-                } else {
-                    error!("MISSING \"{}\"", node.kind().replace("\n", "\\n"));
-                }
-            } else {
-                error!("{}", node.kind());
-            }
-            error!(
-                "[{}, {}] - [{}, {}])",
-                start.row, start.column, end.row, end.column
-            );
-        }
-    }
 
     Ok(())
 }
+
+// fn error_pass(tree: &tree_sitter::Tree, path: &str) -> Result<()> {
+//     let mut cursor = tree.walk();
+
+//     let mut first_error = None;
+//     loop {
+//         let node = cursor.node();
+//         if node.has_error() {
+//             if node.is_error() || node.is_missing() {
+//                 first_error = Some(node);
+//                 break;
+//             } else if !cursor.goto_first_child() {
+//                 break;
+//             }
+//         } else if !cursor.goto_next_sibling() {
+//             break;
+//         }
+//     }
+//
+//     if first_error.is_some() {
+//         error!("{}", path);
+//         if let Some(node) = first_error {
+//             let start = node.start_position();
+//             let end = node.end_position();
+//             error!("\t(");
+//             if node.is_missing() {
+//                 if node.is_named() {
+//                     error!("MISSING {}", node.kind());
+//                 } else {
+//                     error!("MISSING \"{}\"", node.kind().replace("\n", "\\n"));
+//                 }
+//             } else {
+//                 error!("{}", node.kind());
+//             }
+//             error!(
+//                 "[{}, {}] - [{}, {}])",
+//                 start.row, start.column, end.row, end.column
+//             );
+//         }
+//     }
+//     Ok(())
+// }
